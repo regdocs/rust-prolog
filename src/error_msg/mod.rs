d32a03d99801ee63d9f8736ad92ee9d6506a93a1
jayzsh <55305804+jayzsh@users.noreply.github.com>
2024-06-03 01:10:16 +0530

    Add error_msg:: module

---
+++ src/error_msg/mod.rs
@@ -0,0 +1,3 @@
+pub const FAILED_READ_LINE: &str = "Failed to read line";
+pub const FAILED_TO_PARSE_INTEGER: &str = "Failed to parse integer from String";
+pub const FAILED_FLUSH: &str = "Failed to flush stdout";
