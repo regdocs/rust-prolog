535d5e7ef08d8bdcd52599f5244adc05ad564dbd
jayzsh <55305804+jayzsh@users.noreply.github.com>
2024-06-03 01:10:06 +0530

    Add degc_degf:: module

---
+++ src/degc_degf/mod.rs
@@ -0,0 +1,68 @@
+use std::io::Write;
+use crate::error_msg;
+
+fn to_celsius(fahrenheit: f64) -> f64 {
+    5.0 / 9.0 * (fahrenheit - 32.0)
+}
+
+fn to_fahrenheit(celsius: f64) -> f64 {
+    9.0 / 5.0 * celsius + 32.0
+}
+
+pub fn run_module() -> bool {
+    println!("This is degc_degf::run_module() ->");
+    println!(" 1. Convert deg Fahrenheit to deg Celsius");
+    print!("Enter choice or `b` to go back: ");
+    std::io::stdout().flush().unwrap();
+
+    let mut choice: String = String::new();
+    std::io::stdin()
+        .read_line(&mut choice)
+        .expect(error_msg::FAILED_READ_LINE);
+
+    let choice: &str = choice.trim();
+
+    match choice {
+        "1" => {
+            print!("Enter value in deg Fahrenheit: ");
+            std::io::stdout().flush().unwrap();
+            let mut degf: String = String::new();
+            std::io::stdin()
+                .read_line(&mut degf)
+                .expect(error_msg::FAILED_READ_LINE);
+            let degf = degf.trim().parse::<f64>().expect(error_msg::FAILED_TO_PARSE_INTEGER);
+            let degc = to_celsius(degf);
+            println!(
+                "{} deg Fahrenheit is equivalent to {} deg Celsius\n",
+                degf, degc
+            );
+            false
+        }
+
+        "2" => {
+            print!("Enter value in deg celsius: ");
+            std::io::stdout().flush().unwrap();
+            let mut degc: String = String::new();
+            std::io::stdin()
+                .read_line(&mut degc)
+                .expect(error_msg::FAILED_READ_LINE);
+            let degc = degc.trim().parse::<f64>().expect(error_msg::FAILED_TO_PARSE_INTEGER);
+            let degf = to_fahrenheit(degc);
+            println!(
+                "{:.1} deg Celsius is equivalent to {:.1} deg Fahrenheit\n",
+                degc, degf
+            );
+            false
+        }
+
+        "b" => {
+            println!("[GOING BACK...]");
+            true
+        }
+        
+        _ => {
+            print!("Wrong choice, try again!\n\n");
+            false
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::io::Write;
+mod degc_degf;
+mod error_msg;
+
 fn main() {
-    println!("Hello, world!");
+    print!("Hello, World!\n\n");
+
+    'crankshaft: loop {
+        println!("Options:");
+        println!(" 1. degc_degf::run_module()");
+        println!(" 2. fibonacci::run_module()");
+        print!("Enter choice or `q` to exit: ");
+        std::io::stdout().flush().expect(error_msg::FAILED_FLUSH);
+
+        let mut choice: String = String::new();
+        std::io::stdin()
+            .read_line(&mut choice)
+            .expect(error_msg::FAILED_READ_LINE);
+        let choice: &str = choice.trim();
+
+        match choice {
+            "1" => 'module: loop {
+                if !degc_degf::run_module() {
+                    break 'module;
+                }
+            },
+
+            "q" => {
+                println!("\n[EXITING...]");
+                break 'crankshaft;
+            }
+
+            _ => print!("Invalid choice, try again!\n\n"),
+        }
+    }
 }
